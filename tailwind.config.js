9593fa031f741fd90b8804a7c8c173f70a59843a
ricardojosino <ricardojosino@gmail.com>
2022-06-15 21:49:01 -0300

    Foi retirado os css de estilo e deixado tailwind puro

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      './src/index.html',
-      './src/typography.html'
+    './src/index.html',
+    './src/typography.html'
   ],
   theme: {
 
-    colors: {
-      'color01' : '#6666FF',
-      'color02' : '#FF3399',
-      'color03' : '#343434',
-      'color04' : '#F4F4F4',
-      'color05' : '#F3F3F3',
-      'color06' : '#FFFFFF',
+
+    container: {
+      center: true,
     },
 
-    container : {
-      center : true,
-    },
-
-    fontFamily : {
-      'lato' : 'lato'
+    fontFamily: {
+      'lato': 'lato'
     },
 
     extend: {
-      screens : {
-        'xl' : '1140px'
+      screens: {
+        'xl': '1140px'
       },
+
+      colors: {
+        'color01': '#6666FF',
+        'color02': '#FF3399',
+        'color03': '#343434',
+        'color04': '#F4F4F4',
+        'color05': '#F3F3F3',
+        'color06': '#FFFFFF',
+      },
+
     },
   },
   plugins: [],
